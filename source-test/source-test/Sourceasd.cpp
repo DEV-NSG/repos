f2a27e624f3f33303ead3ae13a12f33075bf25f7
NsG <93184355+DEV-NSG@users.noreply.github.com>
2022-06-14 09:47:25 +0100

    update

---
+++ source-test/source-test/Sourceasd.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main() {
+
+	// Create a text file
+	ofstream MyWriteFile("filename3.txt");
+
+	// Write to the file
+	MyWriteFile << "Módulo 7 - PSI";
+
+	// Close the file
+	MyWriteFile.close();
+
+	// Create a text string, which is used to output the text file
+	string myText;
+
+	// Read from the text file
+	ifstream MyReadFile("filename3.txt");
+
+	// Use a while loop together with the getline() function to read the file line by line
+
+	while (getline(MyReadFile, myText)) {
+
+		// Output the text from the file
+
+		cout << myText;
+
+	}
+
+	// Close the file
+
+	MyReadFile.close();
+
+}
---
+++ source-test/source-test/source-test.cpp
@@ -0,0 +1,19 @@
+/*#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main() {
+
+	// Create and open a text file
+	ofstream MyFile("filename2.txt");
+
+	// Write to the file
+
+	MyFile << "Módulo 7 - tratamento de dados";
+
+	// Close the file
+
+	MyFile.close();
+
+}*/
